b096fe987e0835e42aecd52c1ab1a023d23c0bb9
mirlz <mirlzy@gmail.com>
2021-05-07 21:54:09 +0800

    edit codes for heroku deployment

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,10 @@
 console.log('Client side javascript file is loaded.');
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data);
-    });
-});
-
-
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data);
+//     });
+// });
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
@@ -20,7 +18,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = "Loading...";
     messageTwo.textContent = '';
 
-    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+    fetch('/weather?address='+location).then((response) => {
         response.json().then((res) => {
 
             if(res.errorMessage) {
